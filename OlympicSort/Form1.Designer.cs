860eaf10f14a1a5c2cb0f0e5d3f49927a960d1a5
Anastasia <anastasiya.reksius@mail.ru>
2023-10-20 23:32:03 +0700

    Добавьте файлы проекта.

---
+++ OlympicSort/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace OlympicSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Массив: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(637, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(39, 112);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Пузырьковая";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(190, 112);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 20);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Быстрая";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(320, 112);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(102, 20);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Шейкерная";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(480, 112);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(68, 20);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "BOGO";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(607, 112);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(100, 20);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.Text = "Вставками";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(782, 31);
+            this.menuStrip3.TabIndex = 7;
+            this.menuStrip3.Text = "menuStrip3";
+            this.menuStrip3.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip3_ItemClicked);
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(64, 27);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
+            this.calculateToolStripMenuItem.Text = "Рассчитать";
+            this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(176, 30);
+            this.deleteToolStripMenuItem.Text = "Очистить";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Отсортированный массив:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(527, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ OlympicSort/Form1.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+//using Microsoft.Office.Interop.Excel;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OlympicSort
+{
+    public partial class Form1 : Form
+    {
+        private int[] numbers;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       /* private void LoadFromExcelButton_Click(object sender, EventArgs e)
+        {
+            // Code to load data from Excel
+            LoadDataFromExcel();
+        }
+
+        private void LoadFromGoogleSheetsButton_Click(object sender, EventArgs e)
+        {
+            // Code to load data from Google Sheets
+            LoadDataFromGoogleSheets();
+        }
+
+        private void GenerateDataButton_Click(object sender, EventArgs e)
+        {
+            // Code to generate random data
+            GenerateRandomData();
+        }
+
+        private void LoadDataFromExcel()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var excelApp = new Microsoft.Office.Interop.Excel.Application();
+                var workbook = excelApp.Workbooks.Open(openFileDialog.FileName);
+                var worksheet = (Worksheet)workbook.Worksheets[1];
+                var range = worksheet.UsedRange;
+
+                dataGridView.Rows.Clear();
+
+                for (int i = 1; i <= range.Rows.Count; i++)
+                {
+                    // Assuming your DataGridView has columns named "Column1", "Column2", ...
+                    dataGridView.Rows.Add(((Range)range.Cells[i, 1]).Value, ((Range)range.Cells[i, 2]).Value);
+                }
+
+                workbook.Close(false);
+                excelApp.Quit();
+            }
+        }
+
+        private void LoadDataFromGoogleSheets()
+        {
+            string spreadsheetId = "forolympicsort";
+            string range = "Sheet1!A1:B10"; // Range in A1 notation
+
+            // Path to your JSON service account key file downloaded from Google Cloud Console
+            string jsonPath = "C:\\Users\anast\\Downloads\\forolympicsort-c34568779cb0.json";
+
+            var service = GetSheetsService(jsonPath);
+            var values = ReadDataFromSheet(service, spreadsheetId, range);
+
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    Console.WriteLine($"{row[0]}, {row[1]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+        }
+        static SheetsService GetSheetsService(string jsonPath)
+        {
+            var credential = GoogleCredential.FromFile(jsonPath)
+                .CreateScoped(SheetsService.Scope.SpreadsheetsReadonly);
+
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Google Sheets API C# Example",
+            });
+
+            return service;
+        }
+
+        static IList<IList<object>> ReadDataFromSheet(SheetsService service, string spreadsheetId, string range)
+        {
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            ValueRange response = request.Execute();
+            IList<IList<object>> values = response.Values;
+
+            return values;
+        }
+
+        private void GenerateRandomData()
+        {
+            // Generate random data and populate the DataGridView
+            Random random = new Random();
+            dataGridView.Rows.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridView.Rows.Add(random.Next(100), random.Next(100));
+            }
+        }*/
+
+        private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] numbersAsString = textBox1.Text.Split(' ');
+
+            double[] arr = new double[numbersAsString.Length];
+
+            for (int i = 0; i < numbersAsString.Length; i++)
+            {
+                if (double.TryParse(numbersAsString[i], out double number))
+                {
+                    arr[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка преобразования числа: {numbersAsString[i]}");
+                }
+            }
+
+            if (checkBox1.Checked)
+            {
+                BubbleSort(arr);
+            }
+            else if (checkBox5.Checked)
+            {
+                InsertionSort(arr);
+            }
+            else if (checkBox3.Checked)
+            {
+                ShakerSort(arr);
+            }
+            else if (checkBox2.Checked)
+            {
+                textBox2.Text = "Som";
+                //QuickSortAlgorithm(numbers, 0, numbers.Length - 1);
+            }
+            else if (checkBox4.Checked)
+            {
+                BogoSort(arr);
+            }
+            textBox2.Clear();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                textBox2.Text += arr[i].ToString() + " ";
+            }
+        }
+
+        
+        static void BubbleSort(double[] arr, bool ascending = true, Label timeLabel = null)
+        {
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    bool swapRequired = ascending ? arr[j] > arr[j + 1] : arr[j] < arr[j + 1];
+                    if (swapRequired)
+                    {
+                        // Swap arr[j] and arr[j+1]
+                        double temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+           /* stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            timeLabel?.Invoke(new Action(() => timeLabel.Text = $"Bubble Sort Time: {elapsedTime}"));*/
+        }
+
+
+        static void InsertionSort(double[] arr, bool ascending = true, Label timeLabel = null)
+        {
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                double key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1], that are greater (or less) than key,
+                // to one position ahead of their current position
+                while (j >= 0 && ((ascending && arr[j] > key) || (!ascending && arr[j] < key)))
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+            /*stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            timeLabel?.Invoke(new Action(() => timeLabel.Text = $"Bubble Sort Time: {elapsedTime}"));*/
+        }
+
+        /*static void QuickSort(int[] arr, bool ascending = true, Label timeLabel = null)
+        {
+            /*Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            if (low < high)
+            {
+                int partitionIndex = Partition(arr, low, high, ascending);
+
+                QuickSort(arr, low, partitionIndex - 1, ascending);
+                QuickSort(arr, partitionIndex + 1, high, ascending);
+            }
+
+            /*stopwatch.Stop();
+
+            TimeSpan elapsedTime = stopwatch.Elapsed;
+            timeLabel?.Invoke(new Action(() => timeLabel.Text = $"Bubble Sort Time: {elapsedTime}"));
+        }*/
+        public int Partition(double[] arr, int low, int high, bool ascending)
+        {
+            double pivot = arr[high];
+            int i = (low - 1);
+
+            for (int j = low; j < high; j++)
+            {
+                bool swapRequired = ascending ? arr[j] <= pivot : arr[j] >= pivot;
+                if (swapRequired)
+                {
+                    i++;
+                    // Swap arr[i] and arr[j]
+                    double temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // Swap arr[i+1] and arr[high] (or pivot)
+            double temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+        }
+
+        private void ShakerSort(double[] arr)
+        {
+            bool swapped;
+            do
+            {
+                // Проход слева направо, как в обычной сортировке пузырьком
+                swapped = false;
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+
+                // Если не было обменов на этом проходе, массив уже отсортирован,
+                // и следующий проход слева направо не нужен
+
+                swapped = false;
+
+                // Проход справа налево
+                for (int i = arr.Length - 2; i >= 0; i--)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+        static void Swap(double[] arr, int i, int j)
+        {
+            double temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private void BogoSort(double[] arr)
+        {
+            Random random = new Random();
+
+            while (!IsSorted(arr))
+            {
+                Shuffle(arr, random);
+            }
+        }
+        static void Shuffle(double[] arr, Random random)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int randomIndex = i + random.Next(n - i);
+                // Обмен элементов
+                double temp = arr[i];
+                arr[i] = arr[randomIndex];
+                arr[randomIndex] = temp;
+            }
+        }
+
+        static bool IsSorted(double[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < arr[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void menuStrip3_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
